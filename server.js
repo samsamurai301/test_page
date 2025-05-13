b33fd6414817ab8212a7ed18f7c10d2cb9dd2d07
Sohangkumar Patel <71547113+samsamurai301@users.noreply.github.com>
2025-05-13 21:30:30 +0200

    Switch from HTTPS to HTTP server in server.js

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
-const https = require('https');
-const fs = require('fs');
+const http = require('http');
 
-
-// Create a basic HTTPS server
-https.createServer((req, res) => {
+// Create a basic HTTP server
+http.createServer((req, res) => {
   res.writeHead(200);
-  res.end('✅ Hello from secure Node.js HTTPS server on port 5000!\n');
+  res.end('✅ Hello from Node.js HTTP server on port 5000!\n');
 }).listen(5000, '0.0.0.0', () => {
-  console.log('🚀 HTTPS server running at https://0.0.0.0:5000/');
+  console.log('🚀 HTTP server running at http://0.0.0.0:5000/');
 });
